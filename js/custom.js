5e6d6bc9223d9ec7ebf196d6615d9e624107625d
desipossa <c1jang@hanmail.net>
2024-06-17 14:59:59 +0900

    20240617

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,7 @@
 $(function () {
 
     $('#content').fullpage({
-        anchors: ['main', '02'],
+        anchors: ['main', '02', '03'],
         responsiveWidth: 1200,
         responsiveHeight: 700,
         afterLoad: function (name, idx) {
@@ -9,7 +9,15 @@ $(function () {
                 .eq(idx - 1)
                 .addClass('on')
                 .siblings()
-                .removeClass('on')
+                .removeClass('on');
+
+            if (idx == 3) {
+                $('#header').addClass('on');
+                $('#aside').addClass('on');
+            } else {
+                $('#header').removeClass('on');
+                $('#aside').removeClass('on');
+            }
         },
     });
 
@@ -34,6 +42,68 @@ $(function () {
         },
     });
 
+    const mainContentSlide01 = new Swiper('#main_content .tab_slide01', {
+        loop: true,
+        on: {
+            slideChangeTransitionStart: function () {
+                $('#main_content .line').removeClass('on');
+            },
+            slideChangeTransitionEnd: function () {
+                $('#main_content .tab_con .itm01 .list li')
+                    .eq(this.realIndex)
+                    .addClass('on')
+                    .siblings()
+                    .removeClass('on');
+
+                $('#main_content .line').addClass('on')
+            }
+        }
+    });
+
+    $('#main_content .tab_con .itm01 .list li').on('click', function () {
+        let idx = $(this).index();
+        mainContentSlide01.slideToLoop(idx, 400);
+    });
+
+    $('#main_content .itm01 .line .prev').on('click', function () {
+        mainContentSlide01.slidePrev(1000);
+    });
+
+    $('#main_content .itm01 .line .next').on('click', function () {
+        mainContentSlide01.slideNext(1000);
+    })
+
+
+    const mainContentSlide02 = new Swiper('#main_content .tab_slide02', {
+        loop: true,
+        on: {
+            slideChangeTransitionEnd: function () {
+                $('#main_content .tab_con .itm02 .list li')
+                    .eq(this.realIndex)
+                    .addClass('on')
+                    .siblings()
+                    .removeClass('on');
+            }
+        }
+    });
+
+
+
+    $('#main_content .tab_con .itm02 .list li').on('click', function () {
+        let idx = $(this).index();
+        mainContentSlide02.slideToLoop(idx, 400);
+    });
+
+    $('#main_content .itm02 .line .prev').on('click', function () {
+        mainContentSlide02.slidePrev(1000);
+    });
+
+    $('#main_content .itm02 .line .next').on('click', function () {
+        mainContentSlide02.slideNext(1000);
+    })
+
+
+
 
     $('#main_content .tab_menu li button').on('click', function () {
         $(this)
@@ -49,6 +119,8 @@ $(function () {
             .addClass('on')
             .siblings()
             .removeClass('on')
-    })
+    });
+
+
 
 })
